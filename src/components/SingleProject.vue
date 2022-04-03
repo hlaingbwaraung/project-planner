00c2a08aa0727c43baadbcf5c61ecd342cd4eb4a
hbanihon <mizbwar@gmail.com>
2022-04-04 00:32:54 +0900

    1st

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="project">
+    <div class="flexing">
+      <div @click="showDetail = !showDetail">
+        <h3>{{ project.title }}</h3>
+      </div>
+      <div>
+        <i class="bi bi-trash"></i>
+        <i class="bi bi-pen"></i>
+        <i class="bi bi-check2"></i>
+      </div>
+    </div>
+    <p v-if="showDetail">{{ project.detail }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["project"],
+  data() {
+    return {
+      showDetail: false,
+    };
+  },
+};
+</script>
+
+<style>
+.project {
+  padding: 20px;
+  margin: 10px;
+  border-radius: 20px;
+  border-left:5px solid crimson;
+  background: #ececec;
+  box-shadow: 20px 20px 73px #ededed, -20px -20px 73px #ffffff;
+}
+h3 {
+  color: indigo;
+  cursor: pointer;
+}
+.flexing {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+i {
+  margin-left: 15px;
+}
+.bi:hover {
+  cursor: pointer;
+
+  color: crimson !important;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <h1>home</h1>
+    <div v-for="project in projects" :key="project.id">
+      <SingleProject :project="project"></SingleProject>
+    </div>
+  </div>
+</template>
+
+<script>
+import SingleProject from "../components/SingleProject";
+export default {
+  name: "HomeView",
+  components: {
+    SingleProject,
+  },
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/projects")
+      .then((response) => {
+        return response.json();
+      })
+      .then((datas) => {
+        this.projects = datas;
+      })
+      .catch(() => {});
+  },
+};
+</script>
